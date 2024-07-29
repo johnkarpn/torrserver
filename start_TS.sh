91aa2686084672ff939779d3c050c9414f0f81cc
John Karpn <5538918+johnkarpn@users.noreply.github.com>
2024-07-29 13:21:44 +0300

    Refactoring scripts

--- start_TS.sh
+++ start_TS.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
+case $(uname -m) in
+    i386) ARCH="386" ;;
+    i686) ARCH="386" ;;
+    x86_64) ARCH="amd64" ;;
+    aarch64) ARCH="arm64" ;;
+    armv7|armv7l) ARCH="arm7" ;;
+    armv6|armv6l) ARCH="arm5" ;;
+#    armv5|armv5l) ARCH="arm5" ;;
+    *) echo "Unsupported Arch. Can't continue."; exit 1 ;;
+esac
+
+export BIN_NAME="TorrServer-linux-${ARCH}"
+export TS_URL="https://github.com/YouROK/TorrServer/releases/$TS_RELEASE/download/$BIN_NAME"
+
 # Folder for disk cache
-[ ! -d "$TS_TORR_DIR" ] && mkdir -p $TS_TORR_DIR && chmod -R 777 $TS_TORR_DIR
-ln -s $TS_TORR_DIR /torrents && rm $TS_TORR_DIR/torrents
+[ ! -d "$TS_TORR_DIR" ] && mkdir -p "$TS_TORR_DIR" && chmod -R 777 "$TS_TORR_DIR"
+ln -s "$TS_TORR_DIR" /torrents
 
 # Configuration file ts.ini source. Do not change!
-export INI_URL="https://raw.githubusercontent.com/MrKsey/torrserver/main/ts.ini"
-if [ ! -e $TS_CONF_PATH/ts.ini ]; then
-    wget -q --no-check-certificate --user-agent="$USER_AGENT" --content-disposition "$INI_URL" -O $TS_CONF_PATH/ts.ini
-    if [ -e $TS_CONF_PATH/ts.ini ]; then
+export INI_URL="https://raw.githubusercontent.com/johnkarpn/torrserver/main/ts.ini"
+if [ ! -e "$TS_CONF_PATH"/ts.ini ]; then
+    wget -q --no-check-certificate --user-agent="$USER_AGENT" --content-disposition "$INI_URL" -O "$TS_CONF_PATH"/ts.ini
+    if [ -e "$TS_CONF_PATH"/ts.ini ]; then
         echo " "
         echo "============================================="
         echo "$(date): File $TS_CONF_PATH/ts.ini downloaded from the github."
@@ -17,40 +31,27 @@ if [ ! -e $TS_CONF_PATH/ts.ini ]; then
     fi
 fi
 
-if [ -e $TS_CONF_PATH/ts.ini ]; then
-    chmod a+r $TS_CONF_PATH/ts.ini
-    sed -i -e "s/\r//g" $TS_CONF_PATH/ts.ini
-    . $TS_CONF_PATH/ts.ini && export $(grep --regexp ^[a-zA-Z] $TS_CONF_PATH/ts.ini | cut -d= -f1)
+if [ -e "$TS_CONF_PATH/ts.ini" ]; then
+    chmod a+r "$TS_CONF_PATH/ts.ini"
+    sed -i -e "s/\r//g" "$TS_CONF_PATH/ts.ini"
+    . "$TS_CONF_PATH/ts.ini" && export $(grep -i '^[a-z]' "$TS_CONF_PATH/ts.ini" | cut -d= -f1)
     echo "============================================="
     echo "$(date): Configuration settings from ts.ini file:"
     echo " "
-    echo "$(cat $TS_CONF_PATH/ts.ini | grep --regexp ^[a-zA-Z])"
+    grep -i '^[a-z]' "$TS_CONF_PATH/ts.ini"
     echo " "
     echo "============================================="
     echo " "
 fi
 
-# File accs.db source. Do not change!
-export ACCS_URL="https://raw.githubusercontent.com/MrKsey/torrserver/main/accs.db"
-if [ ! -e $TS_CONF_PATH/accs.db ]; then
-    wget -q --no-check-certificate --user-agent="$USER_AGENT" --content-disposition "$ACCS_URL" -O $TS_CONF_PATH/accs.db
-    if [ -e $TS_CONF_PATH/accs.db ]; then
+# File settings.json source. Do not change!
+export CONFIG_URL="https://raw.githubusercontent.com/johnkarpn/torrserver/main/settings.json"
+if [ ! -e "$TS_CONF_PATH/settings.json" ]; then
+    wget -q --no-check-certificate --user-agent="$USER_AGENT" --content-disposition "$CONFIG_URL" -O "$TS_CONF_PATH/settings.json"
+    if [ -e "$TS_CONF_PATH/settings.json" ]; then
         echo " "
         echo "============================================="
-        echo "$(date): File $TS_CONF_PATH/accs.db downloaded from the github."
-        echo "============================================="
-        echo " "
-    fi
-fi
-
-# File config.db source. Do not change!
-export CONFIG_URL="https://raw.githubusercontent.com/MrKsey/torrserver/main/config.db"
-if [ ! -e $TS_CONF_PATH/config.db ]; then
-    wget -q --no-check-certificate --user-agent="$USER_AGENT" --content-disposition "$CONFIG_URL" -O $TS_CONF_PATH/config.db
-    if [ -e $TS_CONF_PATH/config.db ]; then
-        echo " "
-        echo "============================================="
-        echo "$(date): File $TS_CONF_PATH/config.db downloaded from the github."
+        echo "$(date): File $TS_CONF_PATH/settings.json downloaded from the github."
         echo "============================================="
         echo " "
     fi
@@ -65,33 +66,45 @@ if $LINUX_UPDATE ; then
     echo " "
     echo "============================================="
     echo "$(date): Start checking for Linux updates ..."
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update && apt-get upgrade -y && apt-get clean
+    apk update && apk upgrade && apk cache clean
     echo "Finished checking for Linux updates."
     echo "============================================="
     echo " "
+fi
+
+if [ ! -e /TS/TorrServer ]; then
+	wget -q --no-check-certificate -O /TS/TorrServer "$TS_URL"
+    chmod +x /TS/TorrServer
+    if [ -e /TS/TorrServer ]; then
+        echo " "
+        echo "============================================="
+        echo "$(date): $BIN_NAME downloaded from the github."
+        echo "============================================="
+        echo " "
+    fi
 fi
 
 echo " "
 echo "============================================="
 echo "$(date): Starting TorrServer ..."
 echo " "
-/TS/TorrServer --path=$TS_CONF_PATH/ --torrentsdir=$TS_TORR_DIR --port=$TS_PORT $TS_OPTIONS &
-sleep 5
+/TS/TorrServer --path $TS_CONF_PATH/ --torrentsdir $TS_TORR_DIR --port $TS_PORT $TS_OPTIONS &
+echo " "
+sleep 10
 if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
     echo "Current TorrServer file is corrupted or invalid options. Trying to recover."
-    /TS/TorrServer --path=$TS_CONF_PATH/ --torrentsdir=$TS_TORR_DIR --port=$TS_PORT &
+    /TS/TorrServer --path $TS_CONF_PATH/ --torrentsdir $TS_TORR_DIR --port $TS_PORT &
     if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
-        if [ -e $TS_CONF_PATH/backup/TorrServer ]; then
+        if [ -e "$TS_CONF_PATH/backup/TorrServer" ]; then
             rm -f /TS/TorrServer
-            cp -f $TS_CONF_PATH/backup/TorrServer /TS/TorrServer
+            cp -f "$TS_CONF_PATH/backup/TorrServer" /TS/TorrServer
             chmod a+x /TS/TorrServer
-            /TS/TorrServer --path=$TS_CONF_PATH/ --torrentsdir=$TS_TORR_DIR --port=$TS_PORT &
+            /TS/TorrServer --path $TS_CONF_PATH/ --torrentsdir $TS_TORR_DIR --port $TS_PORT &
             sleep 5
             if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
                 echo "Fatal error!!!"
             else
-                echo "Started from backup without options"                
+                echo "Started from backup without options"
             fi
         else
             echo " "
@@ -107,8 +120,6 @@ if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
     fi
 fi
 
-[ "$TS_RELEASE" != "latest" ] && [ $(curl -Is $GIT_URL/tags/$TS_RELEASE | head -n 1 | egrep -o '[0-9]{3}') -eq 200 ] && export TS_URL=$GIT_URL/tags/$TS_RELEASE
-
 env | grep -v cron_task | awk 'NF {sub("=","=\"",$0); print ""$0"\""}' > /TS/cron.env && chmod a+r /TS/cron.env
 
 if $TS_UPDATE ; then
@@ -116,8 +127,7 @@ if $TS_UPDATE ; then
 fi
 
 if [ ! -z "$cron_task" ]; then
-    echo "$cron_task /update_TS.sh >> /var/log/cron.log 2>&1" | crontab -
-    cron -f >> /var/log/cron.log 2>&1&
+    echo "$cron_task /update_TS.sh" | crontab -
     echo " "
     echo "============================================="
     echo "$(date): Cron task set to: $(crontab -l)"
--- update_TS.sh
+++ update_TS.sh
@@ -11,24 +11,6 @@ fi
 mkdir -p /TS/updates
 
 
-# Start checking for ffprobe updates
-echo " "
-echo "=================================================="
-echo "$(date): Start checking for ffprobe updates ..."
-
-wget --no-verbose --no-check-certificate --user-agent="$USER_AGENT" --output-document=/tmp/ffprobe.zip --tries=3 $(\
-curl -s $FFBINARIES | jq '.bin | .[].ffprobe' | grep linux | \
-grep -i -E "$(dpkg --print-architecture | sed "s/amd64/linux-64/g" | sed "s/arm64/linux-arm-64/g" | sed -E "s/armhf/linux-armhf-32/g")" | jq -r)
-unzip -x -o /tmp/ffprobe.zip ffprobe -d /usr/local/bin
-chmod -R +x /usr/local/bin
-echo " "
-ffprobe -version
-echo " "
-echo "Finished checking for ffprobe updates."
-echo "=================================================="
-echo " "
-
-
 # Start checking for blacklist ip updates
 if [ ! -z "$BIP_URL" ]; then
     echo " "
@@ -38,30 +20,30 @@ if [ ! -z "$BIP_URL" ]; then
     cd /TS/updates
     
     EXT=$(basename $(wget -nv --spider --no-check-certificate --user-agent="$USER_AGENT" \
-    --content-disposition "$BIP_URL" -O - 2>&1 | egrep -o 'http.+\w+' | cut -f 1 -d " ") | egrep -o '[^.]*$')
+    --content-disposition "$BIP_URL" -O - 2>&1 | grep -o 'http.+\w+' | cut -f 1 -d " ") | grep -o '[^.]*$')
     
     wget -nv --no-check-certificate --user-agent="$USER_AGENT" \
-    --content-disposition "$BIP_URL" --output-document=bip_raw.$EXT
+    --content-disposition "$BIP_URL" --output-document="bip_raw.$EXT"
     
-    file -b --mime-type bip_raw.$EXT | ( grep -q 'text/plain' && cat bip_raw.$EXT 2>&1 || gunzip -c bip_raw.$EXT) | \
-    egrep -v '^#' | tr -d "[:blank:]" | \
+    file -b --mime-type "bip_raw.$EXT" | ( grep -q 'text/plain' && cat "bip_raw.$EXT" 2>&1 || gunzip -c "bip_raw.$EXT") | \
+    grep -v '^#' | tr -d "[:blank:]" | \
     awk '{gsub("[a-zA-Z][0-9]+\.[0-9]+\.[0-9]+\.[0-9]+","");print}' | \
     awk '{gsub("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+[a-zA-Z]","");print}' | \
-    egrep -o '([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | \
+    grep -o '([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | \
     sed -r 's/0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)/\1.\2.\3.\4/g' | \
-    egrep -v '^(0|22[4-9]|2[3-5]|192\.168\.[0-9]{1,3}\.[0-9]{1,3})' | \
+    grep -v '^(0|22[4-9]|2[3-5]|192\.168\.[0-9]{1,3}\.[0-9]{1,3})' | \
     sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n -u > bip.txt
     
-    rm -f bip_raw.$EXT
+    rm -f "bip_raw.$EXT"
     
     bip_size=$(wc -l bip.txt | cut -f 1 -d ' ')
-    if [ $bip_size -gt 0 ]; then
-        cp -f /TS/updates/bip.txt $TS_CONF_PATH/bip.txt
-        chmod a+r $TS_CONF_PATH/bip.txt
+    if [ "$bip_size" -gt 0 ]; then
+        cp -f /TS/updates/bip.txt "$TS_CONF_PATH/bip.txt"
+        chmod a+r "$TS_CONF_PATH/bip.txt"
         echo "New bip.txt size: $bip_size strings. Restarting TorrServer..."
         pkill TorrServer
         cd $TS_TORR_DIR
-        /TS/TorrServer --path=$TS_CONF_PATH/ --torrentsdir=$TS_TORR_DIR --port=$TS_PORT $TS_OPTIONS &
+        /TS/TorrServer --path $TS_CONF_PATH/ --torrentsdir $TS_TORR_DIR --port $TS_PORT $TS_OPTIONS &
     else
         echo "Error updating blacklist ip from URL - $BIP_URL"
     fi
@@ -75,10 +57,21 @@ fi
 echo " "
 echo "=================================================="
 echo "$(date): Start checking for TorrServer updates ..."
-wget --no-check-certificate --no-verbose --user-agent="$USER_AGENT" --output-document=/TS/updates/TorrServer --tries=3 $(curl -s $TS_URL | \
-egrep -o 'http.+\w+' | \
-grep -i "$(uname)" | \
-grep -i "$(dpkg --print-architecture | tr -s armhf arm7 | tr -s i386 386)"$)
+case $(uname -m) in
+    i386) ARCH="386" ;;
+    i686) ARCH="386" ;;
+    x86_64) ARCH="amd64" ;;
+    aarch64) ARCH="arm64" ;;
+    armv7|armv7l) ARCH="arm7" ;;
+    armv6|armv6l) ARCH="arm5" ;;
+#    armv5|armv5l) ARCH="arm5" ;;
+    *) echo "Unsupported Arch. Can't continue."; exit 1 ;;
+esac
+
+BIN_NAME="TorrServer-linux-${ARCH}"
+TS_URL="https://github.com/YouROK/TorrServer/releases/latest/download/$BIN_NAME"
+
+wget --no-check-certificate --no-verbose --user-agent="$USER_AGENT" --output-document=/TS/updates/TorrServer --tries=3 "$TS_URL"
 
 chmod a+x /TS/updates/TorrServer
 updated_ver=$(/TS/updates/TorrServer --version)
@@ -86,23 +79,23 @@ if [ $? -eq 0 -a ! -z "$updated_ver" ]; then
     current_ver=$(/TS/TorrServer --version)
     if [ "$updated_ver" != "$current_ver" ]; then
         echo "Updating to $updated_ver ..."
-        if [ ! -d $TS_CONF_PATH/backup ]; then
-            mkdir -p $TS_CONF_PATH/backup
+        if [ ! -d "$TS_CONF_PATH/backup" ]; then
+            mkdir -p "$TS_CONF_PATH/backup"
         else
-            [ -e $TS_CONF_PATH/backup/TorrServer ] && rm -f $TS_CONF_PATH/backup/TorrServer
+            [ -e "$TS_CONF_PATH/backup/TorrServer" ] && rm -f "$TS_CONF_PATH/backup/TorrServer"
         fi
         pkill TorrServer
-        cp -f /TS/TorrServer $TS_CONF_PATH/backup/
+        cp -f /TS/TorrServer "$TS_CONF_PATH/backup/"
         cp -f /TS/updates/TorrServer /TS/TorrServer
         chmod a+x /TS/TorrServer
-        /TS/TorrServer --path=$TS_CONF_PATH/ --torrentsdir=$TS_TORR_DIR --port=$TS_PORT $TS_OPTIONS &
+        /TS/TorrServer --path $TS_CONF_PATH/ --torrentsdir $TS_TORR_DIR --port $TS_PORT $TS_OPTIONS &
         sleep 5
         if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
             echo "Error during the update process: downloaded file is corrupted. Restoring backup."
             rm -f /TS/TorrServer
-            cp -f $TS_CONF_PATH/backup/TorrServer /TS/TorrServer
+            cp -f "$TS_CONF_PATH/backup/TorrServer" /TS/TorrServer
             chmod a+x /TS/TorrServer
-            /TS/TorrServer --path=$TS_CONF_PATH/ --torrentsdir=$TS_TORR_DIR --port=$TS_PORT $TS_OPTIONS &
+            /TS/TorrServer --path $TS_CONF_PATH/ --torrentsdir $TS_TORR_DIR --port=$TS_PORT $TS_OPTIONS &
         fi
     else
         echo "Version $current_ver is latest. No update needed."
